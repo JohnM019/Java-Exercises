1b81900a36014e1c6c6703c5555d79c3c7547a30
JohnM019 <johnmariita19@gmail.com>
2024-11-07 21:38:22 +0300

    Integer to hexadecimal

--- src/IntToHex.java
+++ src/IntToHex.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class IntToHex {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number you wish to convert to Hex: ");
+        int num = scan.nextInt();
+        System.out.println("The number " + num + " in Hex is " + intToHex(num));
+    }
+    public static StringBuilder intToHex(int num){
+        if (num == 0) return new StringBuilder("0");
+        StringBuilder res = new StringBuilder();
+        while (num > 0) {
+            res.insert(0, num % 16);
+            num /= 16;
+        }
+        return res;
+    }
 }
